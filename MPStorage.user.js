f8ba6ca7163a9f237e063bdf40d03f961ee5aaf2
Wiripse <wiripse@hfr.lol>
2019-08-05 10:34:06 +0200

    feat: Get username in cookies + safety control on init + cleaning

--- MPStorage.user.js
+++ MPStorage.user.js
@@ -8,7 +8,7 @@ var mpStorage = {
     /* Name of the MP used as MPStorage */
     /* NEVER EVER TOUCH THAT SHIT */
     /* All the 'harmony' between platforms is based on that var */
-    mpName: "a2bcc09b796b8c6fab77058ff8446c34",
+    mpName: 'a2bcc09b796b8c6fab77058ff8446c34',
     /* Recipient of the MPStorage since we can't send MP to ourself */
     mpRecipient: 'MultiMP',
     /* Is the MPStorage's context initiated ? */
@@ -31,12 +31,11 @@ var mpStorage = {
 
         // We look for the username
         mpStorage.getUsername(function (res) {
-            if (res) {
+            if (res && mpStorage.username) {
                 // Then start looking for the MP used as MPStorage
                 mpStorage.findStorageMPOnPage(1, callback);
             }
         });
-
     },
 
     getStorageData: function (callback) {
@@ -66,7 +65,7 @@ var mpStorage = {
         var args = 'content_form=' + encodeURIComponent(JSON.stringify(data));
         args += '&post=' + mpStorage.mpId + '&numreponse=' + mpStorage.mpRepId;
         args += '&pseudo=' + encodeURIComponent(mpStorage.username) + '&cat=prive&verifrequet=1100&sujet=' + encodeURIComponent(mpStorage.mpName);
-        args += '&hash_check=' + mpStorage.getElementByXpath('//input[@name="hash_check"]', document).pop().value;
+        args += '&hash_check=' + mpStorage.getElementByXpath(' //input[@name="hash_check"]', document).pop().value;
 
         // Post request
         mpStorage.loadPage(url, 'post', args, function () {
@@ -91,16 +90,17 @@ var mpStorage = {
             // TODO Wiripse : Traiter les pages suivantes pour gérer le cas où une nouvelle page apparaît pendant qu'on pagine...
             if (mpStorage.pageMax <= 1) {
                 // Save the number of MPs page the user have
-                mpStorage.pageMax = resp.getElementsByClassName('fondForum1PagesHaut')[0].getElementsByClassName('cHeader').length + 1;
+                mpStorage.pageMax = resp.getElementsByClassName('fondForum1PagesHaut')[
+                    0
+                ].getElementsByClassName('cHeader').length + 1;
             }
-
             // Loop on list of MPs
-            [...resp.getElementsByClassName('cCatTopic')].forEach(function (topicRow) {
+            [...resp.getElementsByClassName('cCatTopic')
+            ].forEach(function (topicRow) {
 
                 if (topicRow.title) {
                     if (mpStorage.mpName === topicRow.innerHTML) {
                         // If current MP's name is the one used by MPStorage, baise ouais !
-
                         // We save the MP's topic ID
                         mpStorage.mpId = mpStorage.getJsonFromUrl(topicRow.href).post;
                         // We go after the first response ID
@@ -146,7 +146,15 @@ var mpStorage = {
         mpStorage.loadPage(url, 'get', args, function (resp) {
             // Nice HTML parsing bro !
             // We save the response's ID
-            mpStorage.mpRepId = parseInt(resp.getElementsByClassName('messagetable')[0].getElementsByClassName('messCase1')[0].getElementsByTagName('a')[0].name.split('t')[1]);
+            mpStorage.mpRepId = parseInt(resp.getElementsByClassName('messagetable')[
+                0
+            ].getElementsByClassName('messCase1')[
+                0
+            ].getElementsByTagName('a')[
+                0
+            ].name.split('t')[
+                1
+            ]);
             // And give it to the callback
             callback(mpStorage.mpRepId);
         });
@@ -164,8 +172,37 @@ var mpStorage = {
             data: [
                 {
                     version: '0.1',
-                    mpFlags: {},
-                    blacklist: {},
+                    mpFlags: {
+                        flags: [
+                            {
+                                '123456': {
+                                    'uri': 'https://forum.hardware.fr/forum2.php?config=hfr.inc&cat=prive&post=123456&page=1&p=1&sondage=0&owntopic=0&trash=0&trash_post=0&print=0&numreponse=0&quote_only=0&new=0&nojs=0#t789987',
+                                    'post': 123456,
+                                    'page': 1,
+                                    'href': 't789987',
+                                    'p': '1'
+                                }
+                            }
+                        ],
+                        sourceName: 'HFRGMTools',
+                        lastUpdate: Date.now()
+                    },
+                    blacklist: {
+                        list : [
+                            {
+                                   username : 'MultiMP',
+                                   userId : 99887766,
+                                   createDate : Date.now()
+                            },
+                            {
+                                username : 'MultiMP2',
+                                userId : 66778899,
+                                createDate : Date.now()
+                            }
+                        ],
+                        sourceName : 'HFRGMTools',
+                        lastUpdate : Date.now()
+                    },
                     sourceName: 'HFRGMTools',
                     lastUpdate: Date.now()
                 }
@@ -184,7 +221,6 @@ var mpStorage = {
         mpStorage.loadPage(url, 'post', args, function () {
             callback(true);
         });
-
     },
 
     getUsername: function (callback) {
@@ -194,34 +230,11 @@ var mpStorage = {
         // CALLBACK is called with a boolean giving the result status (true : ok; false : fail)
         // **********
 
-        // We load MPs first page
-        var url = 'https://forum.hardware.fr/forum1.php?config=hfr.inc';
-        var args = '&cat=prive&page=1&subcat=&sondage=0&owntopic=0&trash=0&trash_post=0&moderation=0&new=0&nojs=0&subcatgroup=0';
+        // Can I get your cookie plz ?
+        mpStorage.username = mpStorage.getCookie('md_user');
 
-        // Get request
-        mpStorage.loadPage(url, 'get', args, function (resp) {
-
-            // Find first MP's id
-            var firstMpId = mpStorage.getJsonFromUrl(resp.getElementsByClassName('cCatTopic')[0].href).post;
-
-            // Load the first MP
-            var urlMP = 'https://forum.hardware.fr/forum2.php?config=hfr.inc';
-            var argsMP = '&cat=prive&post=' + firstMpId + '&page=1&p=1&sondage=0&owntopic=0&trash=0&trash_post=0&print=0&numreponse=0&quote_only=0&new=0&nojs=0';
-
-            // Get request
-            mpStorage.loadPage(urlMP, 'get', argsMP, function (resp) {
-                // Parse the shit out of it to find the hidden's input named 'pseudo'
-                [...resp.getElementsByTagName('input')].forEach(function (input) {
-                    if ('pseudo' === input.name) {
-                        // Yeah !
-                        // Save the username
-                        mpStorage.username = input.value;
-                        // And we're done
-                        callback(true);
-                    }
-                });
-            });
-        });
+        // We're done
+        callback(mpStorage.username);
     },
 
     getElementByXpath: function (path, element, doc) {
@@ -232,7 +245,7 @@ var mpStorage = {
 
         if (doc == null) doc = document;
         var arr = Array(), xpr = doc.evaluate(path, element,
-            null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null);
+        null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null);
         for (; item = xpr.iterateNext();) arr.push(item);
         return arr;
     },
@@ -252,26 +265,24 @@ var mpStorage = {
             req.responseType = 'document';
             req.onreadystatechange = mpStorage.processReqChange(req, responseHandler);
             req.open(method, url, true);
-            if (method == 'POST') req.setRequestHeader("Content-Type",
-                "application/x-www-form-urlencoded");
+            if (method == 'POST') req.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
             arguments = method == 'POST' ? arguments : null;
             req.send(arguments);
         }
         else if (window.ActiveXObject) {
             // branch for IE/Windows ActiveX version
-            req = new ActiveXObject("Microsoft.XMLHTTP");
+            req = new ActiveXObject('Microsoft.XMLHTTP');
             if (req) {
                 req.onreadystatechange = mpStorage.processReqChange(req, responseHandler);
                 req.open(method, url, true);
-                if (method == 'POST') req.setRequestHeader("Content-Type",
-                    "application/x-www-form-urlencoded");
+                if (method == 'POST') req.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
                 if (method == 'POST') req.send(arguments);
                 else req.send();
             }
         }
     },
 
-    processReqChange: function (req, responseHandler) {
+    processReqChange: function(req, responseHandler) {
         // **********
         // ToyoAjaxLib
         // Tool method used by loadPage
@@ -292,11 +303,11 @@ var mpStorage = {
                     }
                 }
             }
-            catch (e) { }
+            catch (e) {}
         }
     },
 
-    getJsonFromUrl: function (url) {
+    getJsonFromUrl: function(url) {
         // **********
         // HFRGMTools
         // Tool method to create a JSON based on url's parameters
@@ -306,10 +317,33 @@ var mpStorage = {
         if (!url) url = location.search;
         var query = url.substr(1);
         var result = {};
-        query.split("&").forEach(function (part) {
-            var item = part.split("=");
+        query.split('&').forEach(function (part) {
+            var item = part.split('=');
             result[item[0]] = decodeURIComponent(item[1]);
         });
         return result;
+    },
+
+    getCookie: function(key) {
+        // **********
+        // PetitJean
+        // Get a cookie by its name
+        // RETURN value of the cookie
+        // **********
+
+        if('md_passs' === key) {
+            // We avoid making mistakes
+            return 'Nope';
+        }
+
+        var array = document.cookie.split(';');
+        for (var i = 0; i < array.length; i++) {
+            var k = array[i].substring(0, array[i].indexOf('=')).trim();
+            var v = array[i].substring(1 + array[i].indexOf('='));
+            if (key == k){
+                return v;
+            }
+        }
+        return '';
     }
 }
